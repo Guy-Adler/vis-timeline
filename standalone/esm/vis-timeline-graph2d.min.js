d1eea37db50ac52adc96c0a5c8c0479f8fd37de7
Guy Adler <guyadler2005@gmail.com>
2024-01-06 14:16:50 +0200

    Fix unknown property reverseHorizontalScroll

--- standalone/esm/vis-timeline-graph2d.min.js
+++ standalone/esm/vis-timeline-graph2d.min.js
@@ -5,7 +5,7 @@
  * A fork of vis-timeline. Create a fully customizable, interactive timeline with items and ranges.
  *
  * @version 1.0.0
- * @date    2023-12-15T14:24:22.308Z
+ * @date    2024-01-06T12:12:57.153Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
